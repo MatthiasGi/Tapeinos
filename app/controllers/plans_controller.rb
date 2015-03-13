MatthiasGi/Tapeinos

commit 1bbbade91b7380520fb178697766b27159c59111

Fixed #9

app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@
 class SessionsController < ApplicationController
 
   # Automatically clean the session while logging in.
-  before_action :logout
+  before_action :logout, except: :update
 
   # Remove currently logged in user and server (see below) when creating new
   #    sessions.

config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # Session-management
   get '/login/as/:id' => 'sessions#update', as: 'change_server'
   get '/login/:seed' => 'sessions#temporary', \
-    constraints: { seed: /[a-f0-9]{32}/ }
+    constraints: { seed: /[a-f0-9]{32}/ }, as: 'login_seed'
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'

test/controllers/plans_controller_test.rb
@@ -12,7 +12,7 @@ class PlansControllerTest < ActionController::TestCase
     assert_template 'sessions/new'
 
     user = users(:max)
-    get :index, nil, {user_id: user.id, server_id: user.servers.first.id}
+    get :index, nil, { user_id: user.id, server_id: user.servers.first.id }
     assert_response :success
     assert_template :index
     assert_equal user, assigns(:current_user)
@@ -20,21 +20,10 @@ class PlansControllerTest < ActionController::TestCase
 
   test "allow logged in server" do
     server = servers(:heinz)
-    get :index, nil, {server_id: server.id}
+    get plans_path, nil, @session
     assert_response :success
     assert_template :index
     assert_equal server, assigns(:current_server)
-  end
-
-  test "get register advertising" do
-    server = servers(:heinz)
-    get :index, nil, { server_id: server.id }
-    assert_select '.panel .btn', I18n.t('defaults.register.submit')
-
-    user = users(:max)
-    server = user.servers.first
-    get :index, nil, { user_id: user.id, server_id: server.id }
-    assert_select '.panel .btn', false
   end
 
   test "listing all plans" do

test/integration/user_create_ad_test.rb
@@ -1,0 +1,34 @@
+require 'test_helper'
+
+class UserCreateAdTest < ActionDispatch::IntegrationTest
+
+  test "get register advertising" do
+    server = servers(:heinz)
+    get_via_redirect login_seed_path(server.seed)
+
+    assert_template 'plans/index'
+    assert_select '.panel .btn', I18n.t('defaults.register.submit')
+    get_via_redirect change_server_path(server.siblings.first.id), nil, referer: plans_path
+
+    assert_template 'plans/index'
+    assert_select '.panel .btn', I18n.t('defaults.register.submit')
+  end
+
+  test "no advertise on logged in user" do
+    user = users(:max)
+    post_via_redirect login_path, { user: { email: user.email, password: 'testen' }}
+
+    assert_template 'plans/index'
+    assert_select '.panel .btn', false
+
+    # The following was added due to bug #9
+    assert assigns(:current_server)
+    other = assigns(:current_server).siblings.first
+    get_via_redirect change_server_path(other.id), nil, referer: plans_path
+    assert_template 'plans/index'
+    assert_equal user, assigns(:current_user)
+    assert_equal other, assigns(:current_server)
+    assert_select '.panel .btn', false
+  end
+
+end

commit 3604d42772aa753106ac7266bd414ef6f24c9397

Fixed several bugs introduced with 1bbbade91b7380520fb178697766b27159c59111

app/controllers/plans_controller.rb
@@ -11,4 +11,6 @@ class PlansController < ApplicationController
       .sort_by(&:first_date)
   end
 
+  def edit; end
+
 end

app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@
 class SessionsController < ApplicationController
 
   # Automatically clean the session while logging in.
-  before_action :logout, except: :update
+  before_action :logout
 
   # Remove currently logged in user and server (see below) when creating new
   #    sessions.
@@ -73,6 +73,7 @@ class SessionsController < ApplicationController
     if server and @current_server and @current_server == server or
         @current_server.siblings.include?(server)
       session[:server_id] = server.id
+      @current_user and session[:user_id] = @current_user.id
       redirect_to :back
     else
       flash.now[:invalid_server_change] = true

config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
   delete '/settings' => 'users#destroy'
 
   # Plans-placeholder
-  resources :plans, only: [ :index, :show ]
+  resources :plans, only: [ :index, :edit ]
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products

test/controllers/plans_controller_test.rb
@@ -20,7 +20,7 @@ class PlansControllerTest < ActionController::TestCase
 
   test "allow logged in server" do
     server = servers(:heinz)
-    get plans_path, nil, @session
+    get :index, nil, @session
     assert_response :success
     assert_template :index
     assert_equal server, assigns(:current_server)