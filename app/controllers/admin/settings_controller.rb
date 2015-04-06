980f6e7069ad133cd1aa14e2998ffc7ca8298036
Matthias Gianfelice <matthias@gianfelice.de>
2015-04-06 16:38:11 +0200

    Preparations for #48: make the saving and getting of settings more abstract.

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -1,8 +1,10 @@
-require 'ostruct'
-
 # This controller allows a superadministrator to edit server-specific settings.
 
 class Admin::SettingsController < Admin::AdminController
+
+  # This controller allows the user to change the settings from within the
+  #    administrative interface.
+  include AdjustsSettings
 
   # Only superadministrators are allowed to proceed.
   before_action :require_root
@@ -16,40 +18,12 @@ class Admin::SettingsController < Admin::AdminController
 
   # Saves changes made by the user and displays the new settings.
   def update
-    # Save general settings directly
-    settings = params[:settings].slice(:domain, :redis, :email_server,
-      :email_port, :email_username, :email_email, :email_name, :timezone)
-    SettingsHelper.setHash(settings)
-
-    # The password is only saved if it is not equal to the encoded one sent to
-    #    the client earlier.
-    pw = params[:settings][:email_password]
-    pw.nil? or pw == password_placeholder or
-      SettingsHelper.set(:email_password, pw)
+    # Save the settings via the concern.
+    save_settings
 
     # Reload the settings.
     get_settings
     render :index
-  end
-
-  # ============================================================================
-
-  private
-
-  # Loads all settings and constructs an object for sending to the browser.
-  def get_settings
-    settings = SettingsHelper.getHash.slice(:domain, :redis, :email_server,
-      :email_port, :email_username, :email_email, :email_name, :timezone)
-    @settings = OpenStruct.new(settings)
-    @settings.email_password = password_placeholder
-  end
-
-  # Generates a placeholder for the password with the same length of the current
-  #    one. This is used to send the user a hint about the currently set
-  #    password (i.e. the length). This way the actual password is never sent to
-  #    the user.
-  def password_placeholder
-    " " * SettingsHelper.get(:email_password).size
   end
 
 end
---
+++ app/controllers/concerns/adjusts_settings.rb
@@ -0,0 +1,42 @@
+require 'ostruct'
+
+# This concern allows a controller to support changing the server-settings via
+#    the SettingsHelper.
+
+module AdjustsSettings
+  extend ActiveSupport::Concern
+
+  # ============================================================================
+
+  private
+
+  # Retrieves all relevant settings from the saved file and stores it in an
+  #    object accessable for the view and directly usable for form-helpers.
+  def get_settings
+    settings = SettingsHelper.getHash.slice(:domain, :redis, :email_server,
+      :email_port, :email_username, :email_email, :email_name, :timezone)
+    @settings = OpenStruct.new(settings)
+    @settings.email_password = password_placeholder
+    @settings
+  end
+
+  # Generates a string consisting only of spaces with the length of the
+  #    currently saved password. This is used to mask the password.
+  def password_placeholder
+    " " * SettingsHelper.get(:email_password).size
+  end
+
+  # Saves the transmitted settings by the user.
+  def save_settings
+    # Save general settings at once.
+    settings = params[:settings].slice(:domain, :redis, :email_server,
+      :email_port, :email_username, :email_email, :email_name, :timezone)
+    SettingsHelper.setHash(settings)
+
+    # Only save the new password if it isn't equal the masked string or not set.
+    password = params[:settings][:email_password]
+    password.nil? or password == password_placeholder or
+      SettingsHelper.set(:email_password, password)
+  end
+
+end
